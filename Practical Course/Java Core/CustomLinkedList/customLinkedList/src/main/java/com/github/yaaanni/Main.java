dd916f582a80bec158fe907cd222dae2b4515d19
yaaanni <rw.yana.rw@gmail.com>
2025-10-08 21:53:16 +0300

    list and tests

---
+++ Practical Course/Java Core/CustomLinkedList/customLinkedList/src/main/java/com/github/yaaanni/Main.java
@@ -0,0 +1,34 @@
+package com.github.yaaanni;
+
+import com.github.yaaanni.doublyLinkedList.MyDoublyLinkedList;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        MyDoublyLinkedList<Integer> myDoublyLinkedList = new MyDoublyLinkedList<Integer>();
+//        myDoublyLinkedList.add(0,2);
+//        myDoublyLinkedList.add(0, 4);
+//        myDoublyLinkedList.add(2,7);
+        //        myDoublyLinkedList.addFirst(2);
+//        myDoublyLinkedList.addLast(4);
+//        myDoublyLinkedList.addFirst(3);
+//        myDoublyLinkedList.add(2, 5);
+//        myDoublyLinkedList.remove(2);
+//        System.out.println(myDoublyLinkedList.get(0));
+//        System.out.println(myDoublyLinkedList.get(2));
+//        System.out.println(myDoublyLinkedList.get(1));
+        List<Integer> list = new LinkedList<>();
+
+        list.add(0,1);
+        list.add(0,7);
+        list.add(2,6);
+//        System.out.println(list.remove(0));
+//        myDoublyLinkedList.removeFirst();
+//        System.out.println( myDoublyLinkedList.get(2));
+//        System.out.println(myDoublyLinkedList.get(3));
+
+    }
+}
