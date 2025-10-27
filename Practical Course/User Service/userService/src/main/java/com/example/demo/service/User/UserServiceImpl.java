4f73d142ce8e43ed53b7a100a9f4aabf510a135e
yaaanni <rw.yana.rw@gmail.com>
2025-10-27 18:19:48 +0300

    changed the method for create user

--- Practical Course/User Service/userService/src/main/java/com/example/demo/service/User/UserServiceImpl.java
+++ Practical Course/User Service/userService/src/main/java/com/example/demo/service/User/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.demo.service.User;
 
+import com.example.demo.dto.CardDto;
 import com.example.demo.dto.UserDto;
 import com.example.demo.dto.mapping.CardMapping;
 import com.example.demo.dto.mapping.UserMapping;
@@ -12,6 +13,7 @@ import com.example.demo.repository.JpaUserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -29,7 +31,21 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto createUser(UserDto dto) {
+        List<CardDto> dtoCards = dto.getCards();
+        dto.setCards(null);
         User user = jpaUserRepository.save(userMapping.toEntity(dto));
+        if (dtoCards != null && !dtoCards.isEmpty()) {
+            List<Card> cards = dtoCards
+                    .stream()
+                    .map(c -> {
+                        Card card = cardMapping.toEntity(c);
+                        card.setUser(user);
+                        return card;
+                    })
+                    .toList();
+            jpaCardRepository.saveAll(cards);
+            user.setCards(cards);
+        }
         return userMapping.toDto(user);
     }
 
